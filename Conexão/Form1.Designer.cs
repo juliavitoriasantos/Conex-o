ed5412146a2a6e4b791782ee71086cf70a26bdf3
juliavitoriasantos <cruz.vitoria@aluno.ifsp.edu.br>
2025-05-05 09:10:16 -0300

    Adicionar arquivos de projeto.

---
+++ Conexão/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Conexão
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerrede = new System.Windows.Forms.Timer(this.components);
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.btnverificar = new System.Windows.Forms.Button();
+            this.btnligar = new System.Windows.Forms.Button();
+            this.btndesligar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Location = new System.Drawing.Point(374, 102);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(35, 13);
+            this.lblstatus.TabIndex = 0;
+            this.lblstatus.Text = "label1";
+            this.lblstatus.Click += new System.EventHandler(this.lblstatus_Click);
+            // 
+            // btnverificar
+            // 
+            this.btnverificar.Location = new System.Drawing.Point(330, 255);
+            this.btnverificar.Name = "btnverificar";
+            this.btnverificar.Size = new System.Drawing.Size(112, 62);
+            this.btnverificar.TabIndex = 1;
+            this.btnverificar.Text = "Verificar";
+            this.btnverificar.UseVisualStyleBackColor = true;
+            // 
+            // btnligar
+            // 
+            this.btnligar.Location = new System.Drawing.Point(84, 41);
+            this.btnligar.Name = "btnligar";
+            this.btnligar.Size = new System.Drawing.Size(75, 23);
+            this.btnligar.TabIndex = 2;
+            this.btnligar.Text = "Ligar";
+            this.btnligar.UseVisualStyleBackColor = true;
+            this.btnligar.Click += new System.EventHandler(this.btnligar_Click);
+            // 
+            // btndesligar
+            // 
+            this.btndesligar.Location = new System.Drawing.Point(84, 144);
+            this.btndesligar.Name = "btndesligar";
+            this.btndesligar.Size = new System.Drawing.Size(75, 23);
+            this.btndesligar.TabIndex = 3;
+            this.btndesligar.Text = "Desligar";
+            this.btndesligar.UseVisualStyleBackColor = true;
+            this.btndesligar.Click += new System.EventHandler(this.btndesligar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btndesligar);
+            this.Controls.Add(this.btnligar);
+            this.Controls.Add(this.btnverificar);
+            this.Controls.Add(this.lblstatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerrede;
+        private System.Windows.Forms.Label lblstatus;
+        private System.Windows.Forms.Button btnverificar;
+        private System.Windows.Forms.Button btnligar;
+        private System.Windows.Forms.Button btndesligar;
+    }
+}
+
---
+++ Conexão/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conexão
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            NetworkChange.NetworkAvailabilityChanged += OnNetworkAvailabilityChanged;
+            NetworkChange.NetworkAddressChanged += OnNetworkAddressChanged;
+
+            timerrede.Interval = 10000; // 10 segundos
+            timerrede.Tick += TimerRede_Tick;
+            timerrede.Start();
+        }
+
+        private void OnNetworkAvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
+        {
+            AtualizarStatus($"[EVENTO] Rede disponível: {e.IsAvailable}");
+        }
+
+        private void OnNetworkAddressChanged(object sender, EventArgs e)
+        {
+            AtualizarStatus("[EVENTO] Endereço de rede alterado");
+        }
+
+        private async void TimerRede_Tick(object sender, EventArgs e)
+        {
+            using (Ping ping = new Ping())
+            {
+                try
+                {
+                    var reply = await ping.SendPingAsync("8.8.8.8", 2000);
+                    if (reply.Status == IPStatus.Success)
+                        AtualizarStatus("Conectado à internet");
+                    else
+                        AtualizarStatus("Sem conexão");
+                }
+                catch
+                {
+                    AtualizarStatus("Erro ao verificar conexão");
+                }
+            }
+        }
+
+        private void AtualizarStatus(string mensagem)
+        {
+            lblstatus.Text = mensagem;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            NetworkChange.NetworkAvailabilityChanged -= OnNetworkAvailabilityChanged;
+            NetworkChange.NetworkAddressChanged -= OnNetworkAddressChanged;
+            timerrede.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblstatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnligar_Click(object sender, EventArgs e)
+        {
+            NetworkChange.NetworkAvailabilityChanged += OnNetworkAvailabilityChanged;
+            NetworkChange.NetworkAddressChanged += OnNetworkAddressChanged;
+            timerrede.Start();
+            AtualizarStatus("Monitoramento ativado");
+        }
+
+        private void btndesligar_Click(object sender, EventArgs e)
+        {
+            NetworkChange.NetworkAvailabilityChanged -= OnNetworkAvailabilityChanged;
+            NetworkChange.NetworkAddressChanged -= OnNetworkAddressChanged;
+            timerrede.Stop();
+            AtualizarStatus("Monitoramento desativado");
+        }
+    }
+}
